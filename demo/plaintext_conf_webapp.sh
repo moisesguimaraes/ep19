34951ab2e85a9c73ce8d557fd867bd8c32b5d64a
Moisés Guimarães de Medeiros <guimaraes@pm.me>
2019-07-03 16:10:21 +0200

    lint

--- demo/plaintext_conf_webapp.sh
+++ demo/plaintext_conf_webapp.sh
@@ -4,7 +4,6 @@ echo -e "\nRunning webapp with conf:\n"
 
 cat web_plaintext.conf
 
-echo -e "\n"
-
+echo -e ""
 
 python ../app/app.py --config-file=web_plaintext.conf
--- demo/vault_conf_webapp.sh
+++ demo/vault_conf_webapp.sh
@@ -2,9 +2,9 @@
 
 echo -e "\nRunning webapp with conf:\n"
 
-cat web_vault.conf
+tail -n +1 web_vault.conf castellan.conf mapping.conf
 
-echo -e "\n"
+echo -e ""
 
 OS_VAULT__ROOT_TOKEN_ID=s.FSUHTpovzLBhu4ENA5evXWdC \
 python ../app/app.py --config-file=web_vault.conf
